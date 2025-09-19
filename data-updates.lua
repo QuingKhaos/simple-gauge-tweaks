bad33301fd33deadfbcbc92b424043f10d115f8e
Em Karisch <em@khaos.solutions>
2025-09-19 08:41:42 +0200

    feat: implement unlock technology settings for simple gauge

--- data-updates.lua
+++ data-updates.lua
@@ -1,2 +1,5 @@
 -- Better subgroup placement with SchallCircuitGroup
 require("data-updates.schall-circuit-group")
+
+-- Optionally unlock with Fluid Handling or Steam Power
+require("data-updates.unlock-with-tech")
